801713b434a027dbad965493c1f5e86b05d818f9
DINESHARANI <112246358+DINESHARANI@users.noreply.github.com>
2024-06-09 17:17:48 +0530

    Add files via upload

---
+++ fcs1.java
@@ -0,0 +1,17 @@
+class fcs1
+{
+	static public void main(String[] args)
+	{
+		int x = -12;
+		if(x==0){
+			System.out.println("it is zero");
+		}
+		if(x<0){
+			System.out.println("it is negitive num");
+		}
+		if(x>0){
+			System.out.println("it is positive num");
+		}
+	}
+		
+}
