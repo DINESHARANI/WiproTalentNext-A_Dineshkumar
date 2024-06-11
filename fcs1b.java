23111d0dc0fe0820e338b3c809e38a35efee2d6a
DINESHARANI <112246358+DINESHARANI@users.noreply.github.com>
2024-06-11 09:26:39 +0530

    Add files via upload

---
+++ fcs1b.java
@@ -0,0 +1,17 @@
+public class fcs1b
+{
+    public static void main(String args[])
+    {
+        int a = 7;
+        int b = 27;
+        int c = b%10;
+        if(a==c)
+        {
+            System.out.println("it is true");
+        }
+        else
+        {
+            System.out.println("it is false");
+        }
+    }
+}
